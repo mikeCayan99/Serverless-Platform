2275849940ba7501a74096266c17b30afce12bc5
Mike Cayan <mahirturan9097@gmail.com>
2026-08-13 18:50:29 +0200

    Add storage account module

--- main.tf
+++ main.tf
@@ -4,3 +4,12 @@ module "resource_group" {
   name     = "rg-serverless-lab-dev"
   location = "West Europe"
 }
+
+module "storage_account" {
+  source = "./modules/storage-account"
+
+  name                = "stserverlesslabdev"
+  location            = module.resource_group.resource_group_location
+  resource_group_name = module.resource_group.resource_group_name
+}
+
