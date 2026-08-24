8050e05cf76c98dbe80187eec624ef5fa8db4f54
Mike Cayan <mahirturan9097@gmail.com>
2026-08-24 18:31:29 +0200

    Add Azure Function App infrastructure

---
+++ modules/function-app/main.tf
@@ -0,0 +1,22 @@
+resource "azurerm_service_plan" "service_plan" {
+  name                = var.service_plan_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  os_type  = "Linux"
+  sku_name = "Y1"
+}
+
+resource "azurerm_linux_function_app" "function_app" {
+  name                = var.function_app_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  service_plan_id = azurerm_service_plan.service_plan.id
+
+  storage_account_name       = var.storage_account_name
+  storage_account_access_key = var.storage_account_access_key
+
+  site_config {}
+}
+
